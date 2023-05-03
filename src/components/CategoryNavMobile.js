259efda7022765486f5847a6981dfa48bb1c411e
Jorge Matos <jisapjisap@gmail.com>
2023-05-03 15:18:21 +0200

    Header, mainslider, SearchForm

--- src/components/CategoryNavMobile.js
+++ src/components/CategoryNavMobile.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { FiX } from 'react-icons/fi';
 
-const CategoryNavMobile = () => {
-  return <div>CategoryNavMobile</div>;
+
+const CategoryNavMobile = ({setCatnavMobile}) => {
+  return (
+    <div className='w-full h-full bg-primary p-8'>
+      {/* Close incon */}
+      <div
+        onClick={()=> setCatnavMobile(false)} 
+        className='flex justify-end mb-8 cursor-pointer'
+      >
+        <FiX  className='text-3xl'/>
+      </div>
+      CategoyNavMobile
+    </div>
+  )
 };
 
 export default CategoryNavMobile;
